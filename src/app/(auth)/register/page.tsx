8d953ec44b90077107e16720cfe387d912efc93b
ShibaTTTTTTTTT <39818834+shibaTT@users.noreply.github.com>
2024-10-06 16:35:57 +0900

    fix: delete unused import

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useRef, useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
 import { register } from "@/actions/register";
